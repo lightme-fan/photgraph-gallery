1119879b2270a438f548568666cf107ef628b159
Fanilo Andriamarohaja <fanilo.and@onja.org>
2020-09-10 13:12:58 +0100

    Updated html and image slider

--- webroot/script.js
+++ webroot/script.js
@@ -1,1 +1,13 @@
-console.log('It works')
+const activePic = document.querySelector('.portfolio_image_slider_active');
+console.log(activePic);
+
+const images = document.querySelectorAll('.portfolio_image');
+console.log(images);
+
+images.forEach(image => image.addEventListener('click', (e) => {
+    const clickImage = e.target;
+    if (clickImage) {
+      const image = e.target.closest('.portfolio_image');
+      activePic.innerHTML = image;
+    }
+}))
