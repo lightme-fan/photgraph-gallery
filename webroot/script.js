c3d470a7d3cb40fc5110ca53cebe2454149ffe85
Fanilo Andriamarohaja <fanilo.and@onja.org>
2020-09-11 05:25:49 +0100

    I made sure of the images

--- webroot/script.js
+++ webroot/script.js
@@ -1,13 +1,16 @@
-const activePic = document.querySelector('.portfolio_image_slider_active');
-console.log(activePic);
-
+// Grabbing all images which are not active
 const images = document.querySelectorAll('.portfolio_image');
-console.log(images);
 
-images.forEach(image => image.addEventListener('click', (e) => {
-    const clickImage = e.target;
-    if (clickImage) {
-      const image = e.target.closest('.portfolio_image');
-      activePic.innerHTML = image;
-    }
-}))
+// Grabbing the active image
+const activeImage = document.querySelector('.image_slider_active');
+
+// Handling the slider
+const slider = (e) => {
+  const clickedImage = e.target;
+  if (clickedImage) {
+    activeImage.src = clickedImage.src;
+  }
+}
+
+// Event listner
+images.forEach(image => image.addEventListener(('click'), slider));
