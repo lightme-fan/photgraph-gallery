21585797bff77a0929e046054c198bda2ee57be7
lightme-fan <fanilo.and@onja.org>
2021-12-11 10:59:43 +0300

    Update: added left and right arrow icons to slide the images

--- webroot/script.js
+++ webroot/script.js
@@ -5,6 +5,12 @@ const images = document.querySelectorAll('.portfolio_image');
 const activeImage = document.querySelector('.image_slider_active');
 const firstImage = document.querySelector('.portfolio_image-one');
 const allImages = Array.from(document.querySelectorAll('.picture'))
+const imagesToSlide = Array.from(document.querySelectorAll('.pic'))
+const leftIcon = document.querySelector('.button-slider:nth-of-type(1)')
+const rightIcon = document.querySelector('.button-slider:nth-of-type(2)')
+
+var num = 7;
+var curing = 1;
 
 // Handling the slider
 const slider = (e) => {
@@ -14,6 +20,27 @@ const slider = (e) => {
    images.forEach(image => image.style.opacity = 0.5)
    clickedImage.style.opacity = 1;
 }
+
+const prevImage = () => {
+  if(curing > 1){
+    activeImage.src = imagesToSlide[curing - 1].src;
+    curing = curing - 1;
+  }else{
+    alert("This is the first image");
+  }
+}
+
+const nextImage = () => {
+  if(curing < num){
+    activeImage.src = imagesToSlide[curing + 1].src;
+    curing = curing + 1;
+  }else{
+    alert("This is the last image");
+  }
+}
+
+leftIcon.addEventListener(('click'), prevImage)
+rightIcon.addEventListener(('click'), nextImage)
 
 // Event listeners for images by looping through them
 images.forEach(image => image.addEventListener(('click'), slider));
